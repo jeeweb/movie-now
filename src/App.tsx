03208d3cbd5ab2e08ce3b7737a3dd5eac37f7da6
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-03-23 17:42:51 +0900

    design: favicon 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import { Outlet } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { motion } from "framer-motion";
 import { theme } from "./theme";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   width: 100vw;
-  height: 100vh;
   background: linear-gradient(135deg, #372155, #1e1543);
   color: #fff;
+  overflow-x: hidden;
 `;
 
-const Container = styled.section`
+const Container = styled.div`
   padding: calc(2em + 64px) 2em 2em;
+  overflow-y: auto;
 `;
 
 export default function App() {
@@ -22,10 +24,16 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <Helmet>
         <link
+          rel="icon"
+          type="image/x-icon"
+          href="/favicon.ico"
+          sizes="16x16"
+        />
+        <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&family=Timmana&display=swap"
         />
-        <title>Movie NOW</title>
+        <title>MovieNOW</title>
       </Helmet>
       <Wrapper>
         <GlobalStyle />
